05d8b8697653f99ba0036074258bee2f627e9de1
bahnos <mara.bahnik@gmail.com>
2023-12-14 22:06:55 +0100

    removed old code

--- source/segmentation.py
+++
@@ -1,8 +0,0 @@
-import cv2
-
-
-def perform_segmentation(frame, prev_frame):
-    cv2.imshow("current frame", frame)
-    if prev_frame:
-        cv2.imshow("prev frame", prev_frame)
-    return frame
